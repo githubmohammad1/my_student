1495cde8187e5f717a60503fcc13dce88675963f
mohammad edrees <mohammadsaeededrees0@gmail.com>
2025-08-31 09:27:59 +0300

    إبقاء الملفت الضرورية

--- quran_school/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Student, Test, Attendance, Announcement, Payment,Progress
-
-admin.site.register(Student)
-admin.site.register(Test)
-admin.site.register(Attendance)
-admin.site.register(Announcement)
-admin.site.register(Payment)
-admin.site.register(Progress)
--- quran_school/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuranSchoolConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quran_school'
